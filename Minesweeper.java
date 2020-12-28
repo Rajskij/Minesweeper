2a44e607726649b46bf3266f4fa02674c928433d
Igor <68941665+Rajskij@users.noreply.github.com>
2020-12-28 15:42:44 +0200

    release v0.4

---
+++ Minesweeper.java
@@ -0,0 +1,67 @@
+package minesweeper;
+
+import java.util.Random;
+
+public class Minesweeper {
+    private final String[][] arr;
+    private final int n;
+
+    public Minesweeper(String[][] arr, int n) {
+        this.arr = arr;
+        this.n = n;
+    }
+
+    public void printField () {
+        for (String[] i : arr) {
+            for (String j : i) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public String[][] fillField () {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = ".";
+            }
+        }
+        return arr;
+    }
+    public String[][] placeMines () {
+        Random random = new Random();
+        int l = 0;
+        while (l < n) {
+            int a = random.nextInt(9);
+            int b = random.nextInt(9);
+            if (arr[a % arr.length][b % arr.length].equals(".")) {
+                arr[a % arr.length][b % arr.length] = "X";
+                l++;
+            }
+        }
+        return arr;
+    }
+    public String[][] chekMines () {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (!arr[i][j].equals("X")) {
+                    int count = getNumMines(i,j);
+                    arr[i][j] = count > 0 ? count + "" : ".";
+                }
+            }
+        }
+        return arr;
+    }
+    public int getNumMines (int x, int y) {
+        int num = 0;
+        for (int i = x - 1; i < x + 2; i++) {
+            if (i >= 0 && i < arr.length) {
+                for (int j = y - 1; j < y + 2; j++) {
+                    if (j >= 0 && j < arr.length && arr[i][j].equals("X")) {
+                        num++;
+                    }
+                }
+            }
+        }
+        return num;
+    }
+}
